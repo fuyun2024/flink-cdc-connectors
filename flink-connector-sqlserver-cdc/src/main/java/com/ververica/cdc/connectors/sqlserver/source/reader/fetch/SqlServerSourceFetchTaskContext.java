486687591dc3c8835029e4f09fe552aab0622f8c
ehui <374488688@qq.com>
2023-03-24 00:48:13 +0800

    [sqlserver] Fix old change data that will be captured when the latest mode starts

--- flink-connector-sqlserver-cdc/src/main/java/com/ververica/cdc/connectors/sqlserver/source/reader/fetch/SqlServerSourceFetchTaskContext.java
+++ flink-connector-sqlserver-cdc/src/main/java/com/ververica/cdc/connectors/sqlserver/source/reader/fetch/SqlServerSourceFetchTaskContext.java
@@ -163,6 +163,7 @@ public class SqlServerSourceFetchTaskContext extends JdbcSourceFetchTaskContext 
                         : sourceSplitBase.asStreamSplit().getStartingOffset();
 
         SqlServerOffsetContext sqlServerOffsetContext = loader.load(offset.getOffset());
+        sqlServerOffsetContext.preSnapshotCompletion();
         return sqlServerOffsetContext;
     }
 
